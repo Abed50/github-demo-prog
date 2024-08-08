c440bcd2889fc6ffdbfe3c49bb67afd284b94f62
Abed Nabil <A.abuhelwe@gmail.com>
2024-08-08 20:46:10 +0300

    add a list compo to the py file

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ print(squared)
 #Add Comment on Python
 
 class Animal:
-    def __init__(name,breed):
+    def __init__(name,age):
         this.name = name
-        this.breed = breed
+        this.age = age
+
+x = [x * x for x in range(20)]
+print(x)
